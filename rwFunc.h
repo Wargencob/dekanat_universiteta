9c012801d272165c903d666c6a45a565bfffda43
Wargencob <gleb.vasilev.14@bk.ru>
2025-04-17 15:59:20 +0000

    add prototipe of rwFunc and menu

--- rwFunc.h
+++ rwFunc.h
@@ -1,4 +1,13 @@
 #ifndef RWFUNC_H
 #define RWFUNC_H
 
+#include <vector>
+#include <string>
+
+std::vector<std::string> readTextFromFile(std::string filePath);
+
+void writeTextInFile(std::vector<std::string>);
+
+std::vector<std::string> split(std::string text);
+
 #endif // RWFUNC_H
